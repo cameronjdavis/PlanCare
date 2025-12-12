7154935453dd7446ded4afa1c1ed8ac2c1077267
Cam <cameronjdavis@users.noreply.github.com>
2025-12-13 07:35:47 +0800

    Using react router

---
+++ spaplancare/src/components/RegoCheck.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function CarTable() {
+    const [data, setData] = useState([
+    ]);
+    useEffect(() => {
+        axios.get('/api/Car')
+            .then(res => setData(res.data))
+            .catch(err => console.error(err));
+    }, [])
+
+    return (
+            <table>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Make</td>
+                        <td>Rego</td>
+                        <td>Registered Until</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((car, i) => {
+                            return <tr key={i}>
+                                <td>{car.id}</td>
+                                <td>{car.make}</td>
+                                <td className="rego">{car.registration}</td>
+                                <td>{car.registeredUntil}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+    )
+}
+
+export default CarTable;
--- spaplancare/src/main.jsx
+++ spaplancare/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, NavLink } from "react-router";
 import './index.css'
-import App from './App.jsx'
+import CarTable from './components/CarTable';
+import RegoCheck from './components/RegoCheck';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+        <BrowserRouter>
+            <p><NavLink to="/">Cars in a table</NavLink></p>
+            <p><NavLink to="/registration">Rego Check</NavLink></p>
+            <Routes>
+                <Route path="/" element={<CarTable />} />
+                <Route path="/registration" element={<RegoCheck />} />
+            </Routes>
+        </BrowserRouter>
   </StrictMode>,
 )
