822d4bd151ff56e8a318aa1ce400d74adc02a8c9
Cam <cameronjdavis@users.noreply.github.com>
2025-12-12 13:07:15 +0800

    API controller to get list of cars and filter optionally by make

---
+++ PlanCare/Controllers/CarController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PlanCare.Model;
+
+namespace PlanCare.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Car>>> GetCars([FromQuery(Name = "make")] string make = "") {
+            using StreamReader r = new StreamReader("cars.json");
+            string json = r.ReadToEnd();
+            var cars = JsonConvert.DeserializeObject<List<Car>>(json) ?? new List<Car>();
+            if (make.Length > 0) {
+                cars = cars.Where(c => c.Make.ToLower().Equals(make)).ToList();
+            }
+            return Ok(cars);
+        }
+    }
+}
---
+++ PlanCare/Model/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanCare.Model
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; } = String.Empty;
+        public string Registration { get; set; } = String.Empty;
+        public DateOnly RegistrationDate { get; set; }
+    }
+}
